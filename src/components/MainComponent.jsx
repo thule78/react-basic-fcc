f8f96289b195ab9cc9ea5b4284b4f3853153df41
thule <lunthu@gmail.com>
2019-05-01 17:31:21 +0700

    add javascript for time of date

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
 function MainComponent (){
+  const date = new Date()
+  const yyyy =  date.getFullYear()
+  const mm = ("0"+(date.getMonth()+1)).slice(-2)
+  const dd = ("0"+date.getDate()).slice(-2)
+  const getdate = dd+'-'+mm+'-'+yyyy
+  const hours = date.getHours()
+  let timeOfDay
+    if (hours < 12){
+      timeOfDay = "Morning"
+    } else if (hours >=12 && hours < 17){
+      timeOfDay = "Afternoon"
+    }else {
+      timeOfDay = "Evening"
+    }
+
+
   return (
     <div>
-      <h1>Todo List</h1>
+    <p>Today: {`${getdate}`}</p>
+      <h3>Your todo list of this {`${timeOfDay}`}</h3>
       <div>
         <input type="checkbox" />
         <p> 2 hours lesson </p>

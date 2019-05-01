41cdc642fc23baf4952117e327cb1d3626828faf
thule <lunthu@gmail.com>
2019-05-01 16:04:43 +0700

    first parent child component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+function Navbar (){
+  return(
+    <div>
+      <nav>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </nav>
+    </div>
+
+    )
+}
+
+export default Navbar
